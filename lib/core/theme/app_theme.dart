01a13f3f31e4e415aa9ba6e4ac7748a8e9c57398
m7skoo <m7skoo.88@gmail.com>
2024-04-18 13:08:00 +0300

    update themeData

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -19,13 +19,4 @@ class AppTheme {
     colorScheme: ColorScheme.light(secondary: kLightSecondaryColor)
         .copyWith(background: kLightBackgroundColor),
   );
-
-  // static final darkTheme = ThemeData(
-  //     brightness: Brightness.dark,
-  //     primaryColor: darkPrimaryColor,
-  //     visualDensity: VisualDensity.adaptivePlatformDensity,
-  //     textButtonTheme: TextButtonThemeData(
-  //         style: TextButton.styleFrom(foregroundColor: darkTextColor)),
-  //     colorScheme: ColorScheme.light(secondary: lightSecondaryColor)
-  //         .copyWith(background: darkBackgroundColor));
 }
